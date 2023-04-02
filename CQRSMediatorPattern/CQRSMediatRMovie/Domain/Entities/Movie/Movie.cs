1a0ef8fb9f3ada55eea49c8d996fb8fd8b98c2d7
Phystro <phystroq@gmail.com>
2023-04-02 18:34:24 +0300

    add: add contructor

--- CQRSMediatorPattern/CQRSMediatRMovie/Domain/Entities/Movie/Movie.cs
+++ CQRSMediatorPattern/CQRSMediatRMovie/Domain/Entities/Movie/Movie.cs
@@ -2,6 +2,14 @@ namespace CQRSMediatRMovie.Domain.Entities.Movie
 {
     public class Movie
     {
+        public Movie(string? title, string? description, MovieGenre genre, int? rating)
+        {
+            Title = title;
+            Description = description;
+            Genre = genre;
+            Rating = rating;
+        }
+
         public long Id { get; set; }
         public string? Title {  get; set; }
         public string? Description { get; set; }
