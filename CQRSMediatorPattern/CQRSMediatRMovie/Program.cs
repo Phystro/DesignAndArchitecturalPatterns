2e6689c605fccdfcafa9246764e85306c27dc4a7
Phystro <phystroq@gmail.com>
2023-04-03 02:10:24 +0300

    add: whole project

--- CQRSMediatorPattern/CQRSMediatRMovie/Program.cs
+++ CQRSMediatorPattern/CQRSMediatRMovie/Program.cs
@@ -3,6 +3,11 @@ global using CQRSMediatRMovie.Domain.Entities.Movie;
 global using Microsoft.EntityFrameworkCore;
 global using MediatR;
 global using CQRSMediatRMovie.Repository.Context;
+global using CQRSMediatRMovie.Domain.DTOs.Requests.Movie;
+global using CQRSMediatRMovie.Domain.DTOs.Responses.Movie;
+global using CQRSMediatRMovie.Application.Movies.Commands.CreateMovie;
+global using CQRSMediatRMovie.Application.Movies.Queries.GetMovies;
+global using CQRSMediatRMovie.Application.Movies.Queries.GetMovie;
 
 var builder = WebApplication.CreateBuilder(args);
 
